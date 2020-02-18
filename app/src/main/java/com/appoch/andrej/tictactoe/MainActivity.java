8c97ab08f45423118fbc86cedc1c80e91609cd07
Anto <andrej.antevski99@gmail.com>
2020-02-18 16:51:57 +0100

    Tic Tac Toe PvP local

---
+++ app/src/main/java/com/appoch/andrej/tictactoe/MainActivity.java
@@ -0,0 +1,22 @@
+package com.appoch.andrej.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+        ImageView image = (ImageView) view;
+
+        image.setX(-100);
+        image.animate().translationXBy(100).alpha(1).setDuration(2000);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
